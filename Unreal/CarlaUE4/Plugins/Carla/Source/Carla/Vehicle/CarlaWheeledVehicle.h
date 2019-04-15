502235302fa2c91ce2d71fa63011f440fa64aa12
Manish <manish.thani@gmail.com>
2019-04-15 13:02:36 +0200

    setting tire config during begin play

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/CarlaWheeledVehicle.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Vehicle/CarlaWheeledVehicle.h
@@ -173,6 +173,15 @@ public:
     SetHandbrakeInput(false);
   }
 
+  /// @}
+  // ===========================================================================
+  /// @name Overriden from AActor
+  // ===========================================================================
+  /// @{
+protected:
+
+  virtual void BeginPlay() override;
+
 private:
 
   /// Current state of the vehicle controller (for debugging purposes).
